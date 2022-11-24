0913960c214f80673a20593f3836d9b78a0509ce
kavitha-kavee <kavithathiruvengadam29@gmail.com>
2022-11-24 20:25:34 +0530

    hashMap

---
+++ JavaPro/src/javaCoder/Base.java
@@ -0,0 +1,22 @@
+package javaCoder;
+
+import java.util.HashMap;
+
+public class Base {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i = 0;
+		 HashMap<Integer, String> info = new HashMap<>();
+		 info.put(1, "kate");
+		 info.put(2, "yeast");
+		 info.put(3, "beast");
+		 System.out.print("info"+info);
+		 info.toString();
+		 System.out.print("to String-->"+info.toString()+" getClass-->"+info.getClass());
+		  
+		 
+		 
+	}
+
+}
